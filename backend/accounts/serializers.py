f36fc07579dba806fd8149a415b2f8bf921ca415
Pratul Kumar <pratulkumar2003@gmail.com>
2026-04-22 00:39:52 +0530

    Modify accounts's serializers.py

--- backend/accounts/serializers.py
+++ backend/accounts/serializers.py
@@ -6,13 +6,13 @@ class RegisterSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = User
-        fields = ['username', 'email', 'password', 'role']
+        fields = ['username', 'email', 'password']
 
     def create(self, validated_data):
         user = User.objects.create_user(
             username=validated_data['username'],
             email=validated_data['email'],
             password=validated_data['password'],
-            role=validated_data.get('role', 'USER')
+            role='USER'
         )
         return user
